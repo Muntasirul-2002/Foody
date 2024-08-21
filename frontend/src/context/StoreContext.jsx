6ec1f2108d8a1d7d139982df56b7325a73ef6442
Muntasirul islam <108189526+Muntasirul-2002@users.noreply.github.com>
2024-08-21 23:05:00 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://foody-backend-k0z8.onrender.com";
+  const url = "https://foody-backend-pkhb.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
